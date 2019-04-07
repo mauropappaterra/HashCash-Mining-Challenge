61da39439842c63283423485679372c65613eb35
eddie4ever <mauropappaterra@gmail.com>
2019-04-07 22:01:15 +1000

    first push

---
+++ README.md
@@ -0,0 +1,1 @@
+# HashCash Mining Challenge
---
+++ brute_force_sha256.py
@@ -0,0 +1,15 @@
+# HashCash Mining Challenge
+# brute_force_sha256.py
+# Created by Mauro J. Pappaterra on 07 of April 2019.
+
+def new_nonce():
+    nonce='xx'
+    return nonce
+
+def find_hash (student_id):
+    solution = new_nonce()
+    return solution
+
+print(find_hash('45648766'))
+
+
