40130fcb1972b7db7c060778fdbd23583ed1f9eb
eddie4ever <mauropappaterra@gmail.com>
2019-04-07 22:53:44 +1000

    added cryptographic solution with tests and printouts

--- brute_force_sha256.py
+++ brute_force_sha256.py
@@ -1,15 +1,42 @@
 # HashCash Mining Challenge
 # brute_force_sha256.py
 # Created by Mauro J. Pappaterra on 07 of April 2019.
-
-def new_nonce():
-    nonce='xx'
-    return nonce
+from datetime import datetime as t
+import hashlib as c
+import random as r
+import string as s
 
 def find_hash (student_id):
-    solution = new_nonce()
-    return solution
+    attemps = 0
+    flag = False
+    start_time = t.now()
+
+    while (not flag):
+
+        # Create nonce
+        nonce = ''.join(r.choices(s.ascii_letters + s.digits, k=r.randint(1, 20)))
+        #print(nonce)
+
+        # Hash it!
+        hash = c.sha256((student_id + nonce).encode()).hexdigest()
+        #print(hash)
+        #print(hash[:7])
+
+        # FOR TESTING PURPOSES
+        if (attemps == 1500):
+            hash = '000000000000000000000'
+
+        #Check if solution is right
+        print("Compare " + hash[:7] + " with '0000000'")
+        if (hash[:7] == '0000000'):
+            flag = True
+            total_time = start_time - t.now()
+
+            print ("\nSolution Found!\nHash -> " + hash + "\nNonce -> " + nonce + "\nNumber of Attemps Needed -> " + str(attemps) + "\nTime Needed -> " + str(total_time))
+        else:
+            attemps += 1
+
+    return "nothing"
+
 
 print(find_hash('45648766'))
-
-
