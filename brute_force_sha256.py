41d2411808d13cbddc80702b0f313d9890ff4b07
eddie4ever <mauropappaterra@gmail.com>
2019-04-08 21:25:13 +1000

    updated challenge to 11 zeores

--- brute_force_sha256.py
+++ brute_force_sha256.py
@@ -1,7 +1,6 @@
 # HashCash Mining Challenge
 # brute_force_sha256.py
 # Created by Mauro J. Pappaterra on 07 of April 2019.
-
 import datetime as t
 import hashlib as c
 import random as r
@@ -19,22 +18,17 @@ def find_hash (student_id):
         nonce = ''.join(r.choices(s.ascii_letters + s.digits, k=r.randint(1, 20)))
         # Hash it!
         hash = c.sha256((student_id + nonce).encode()).hexdigest()
-
-        # FOR TESTING PURPOSES
-        #if (attemps == 200000):
-        #    hash = '000000000'
-
-        if (hash[:7] == '0000000'): #Check solution
+        # Check solution
+        if (hash[:11] == '00000000000'):
             flag = True
-            total_time = t.datetime.now()- start_time
-            solution = (hash, nonce, attemps, total_time)
+            solution = (nonce, hash, attemps, t.datetime.now() - start_time)
         else:
             attemps += 1
 
     return solution
 
 my_solution = find_hash('45648766')
-message = "SOLUTION FOUND!\nHash -> " + my_solution[0] + "\nNonce -> " + my_solution[1] + "\nNumber of Attemps Needed -> " + str(my_solution[2]) + "\nTime Needed -> " + str(my_solution[3])
+message = "SOLUTION FOUND!\nNonce -> " + my_solution[0] + "\nHash -> " + my_solution[1] + "\nNumber of Attemps Needed -> " + str(my_solution[2]) + "\nTime Needed -> " + str(my_solution[3])
 
 with open (my_solution[1] + ".txt", 'w') as file: # Save solution to external file
     file.write (message)
