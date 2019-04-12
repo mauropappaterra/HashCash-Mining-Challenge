1abaa1541db792644031c0397635ecd13dfabb4c
eddie4ever <mauropappaterra@gmail.com>
2019-04-12 11:58:30 +1000

    made code more generic, general refactoring

--- brute_force_sha256.py
+++ brute_force_sha256.py
@@ -6,12 +6,12 @@ import hashlib as c
 import random as r
 import string as s
 
-def find_hash (student_id):
-    attemps = 0
+def find_hash (student_id, zeroes):
+    attempts = 0
     flag = False
     start_time = t.datetime.now()
 
-    print ("Finding nonce for " + student_id + "...")
+    print ("Finding a nonce for student id '" + student_id + "' resulting in a sha256 hash that begins with at least " + str(zeroes) + " zeroes...")
 
     while (not flag):
         # Create nonce
@@ -19,16 +19,17 @@ def find_hash (student_id):
         # Hash it!
         hash = c.sha256((student_id + nonce).encode()).hexdigest()
         # Check solution
-        if (hash[:11] == '00000000000'):
+        if (hash[:zeroes] == '0' * zeroes):
             flag = True
-            solution = (nonce, hash, attemps, t.datetime.now() - start_time)
+            solution = (nonce, hash, attempts, t.datetime.now() - start_time)
         else:
-            attemps += 1
+            attempts += 1
 
     return solution
 
-my_solution = find_hash('45648766')
-message = "SOLUTION FOUND!\nNonce -> " + my_solution[0] + "\nHash -> " + my_solution[1] + "\nNumber of Attemps Needed -> " + str(my_solution[2]) + "\nTime Needed -> " + str(my_solution[3])
-
-with open (my_solution[1] + ".txt", 'w') as file: # Save solution to external file
-    file.write (message)
+while (True):
+    my_solution = find_hash('45648766', 9)
+    message = "SOLUTION FOUND!\nNonce -> " + my_solution[0] + "\nHash -> " + my_solution[1] + "\nNumber of Attemps Needed -> " + str(my_solution[2]) + "\nTime Needed -> " + str(my_solution[3])
+    print(message)
+    with open (my_solution[1] + ".txt", 'w') as file: # Save solution to external file
+        file.write (message)
